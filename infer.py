318e07b7f262a9084bb9e0cffe8176b97d6f757a
Brian Robinson <bigrobinson@gmail.com>
2020-04-28 15:12:40 -0500

    Add ability to specify 256x256 chip
    Pass top left row and column indices of image chip of interest (to do change detection of desired region of larger image)

--- infer.py
+++ infer.py
@@ -38,16 +38,19 @@ class Colorization:
 
 class ChangeDetect:
 
-    def __init__(self, img0_path, img1_path, out_dir, model_path, use_corr=True):
-        
+    def __init__(self, img0_path, img1_path, img_row, img_col, out_dir, model_path, use_corr=True):
+
         self.img0_path = img0_path
         self.img1_path = img1_path
-        self.use_corr = use_corr
+        self.img_row = img_row
+        self.img_col = img_col
         self.out_dir = out_dir
-        
-    
+        self.model_path = model_path
+        self.use_corr = use_corr
+
+
     def preprocess_image(self):
-           
+
         if os.path.isfile(self.img0_path) == False:
             print ('Error: File Not Found: ' + self.img0_path)
             exit(-1)
@@ -57,31 +60,30 @@ class ChangeDetect:
 
         img0 = cv2.imread(self.img0_path, cv2.IMREAD_COLOR)
         img1 = cv2.imread(self.img1_path, cv2.IMREAD_COLOR)
-        
-        # Images must be 256 x 256       
+
+        # Images must be 256 x 256
         img0_ = np.asarray(img0).astype("f").transpose(2, 0, 1) / 128.0 - 1.0
         img1_ = np.asarray(img1).astype("f").transpose(2, 0, 1) / 128.0 - 1.0
-        
-        #### TEMP #########
-        img0_ = img0_[:,1100:1100+256, 2090:2090+256]
-        img1_ = img1_[:,1100:1100+256, 2090:2090+256]
-        ###################
-        
+
+        # Cut out image 256 x 256 chip
+        img0_ = img0_[:, self.img_row:self.img_row+256, self.img_col:self.img_col+256]
+        img1_ = img1_[:, self.img_row:self.img_row+256, self.img_col:self.img_col+256]
+
         input_ = torch.from_numpy(np.concatenate((img0_, img1_), axis=0))
-        
+
         print(input_.shape) # returns torch.size([6, 256, 256])
 
         input_ = input_[np.newaxis, :, :]
         input_ = input_.cuda()
         input_ = Variable(input_)
-        
+
         return input_
-    
-    
-    def run_inference(self, input_, model_path):
-        
+
+
+    def run_inference(self, input_):
+
         self.color_transform = Colorization(2)
-        
+
         # pretrained keyword refers to resnet feature detector being pretrained
         if self.use_corr:
             print('Correlated Siamese Change Detection Network (CSCDNet)')
@@ -91,14 +93,14 @@ class ChangeDetect:
             print('Siamese Change Detection Network (Siamese CDResNet)')
             self.model = cscdnet.Model(inc=6, outc=2, corr=False, pretrained=True)
 
-        if os.path.isfile(model_path) is False:
-            print("Error: Cannot read file ... " + model_path)
+        if os.path.isfile(self.model_path) is False:
+            print("Error: Cannot read file ... " + self.model_path)
             exit(-1)
         else:
-            print("Reading model ... " + model_path)
-        
+            print("Reading model ... " + self.model_path)
+
         # Load trained model (from dataparallel module if necessary)
-        state_dict = torch.load(model_path)
+        state_dict = torch.load(self.model_path)
         first_pair = next(iter(state_dict.items()))
         if first_pair[0][:7] == "module.":
             # create new OrderedDict with generic keys
@@ -109,57 +111,60 @@ class ChangeDetect:
                 new_state_dict[name] = v
             # load params
             self.model.load_state_dict(new_state_dict)
-        else:       
+        else:
             self.model.load_state_dict(state_dict)
-        
+
         self.model = self.model.cuda()
-        
+
         output_ = self.model(input_)
-  
+
         inputs = input_[0].cpu().data
         img0 = inputs[0:3, :, :]
         img1 = inputs[3:6, :, :]
         img0 = (img0 + 1.0) * 128
         img1 = (img1 + 1.0) * 128
-      
+
         output = output_[0][np.newaxis, :, :, :]
         output = output[:, 0:2, :, :]
         mask_pred = np.transpose(self.color_transform(output[0].cpu().max(0)[1][np.newaxis, :, :].data).numpy(),
                                  (1, 2, 0)).astype(np.uint8)
-                
+
         img_out = self.display_result(img0, img1, mask_pred)
-        
+
         return mask_pred, img_out
-    
+
     def display_result(self, img0, img1, mask_pred):
-        
+
         rows = cols = 256
         img_out = np.zeros((rows, cols * 3, 3), dtype=np.uint8)
         img_out[0:rows, 0:cols, :] = np.transpose(img0.numpy(), (1, 2, 0)).astype(np.uint8)
         img_out[0:rows, cols:cols * 2, :] = np.transpose(img1.numpy(), (1, 2, 0)).astype(np.uint8)
         img_out[0:rows, cols*2:cols*3, :] = mask_pred
 
-        img_filename, _ = os.path.splitext(os.path.basename(self.img0_path))        
-        img_save_path = os.path.join(self.out_dir, '{}.png'.format(img_filename))
+        img_filename, _ = os.path.splitext(os.path.basename(self.img0_path))
+        img_save_path = os.path.join(self.out_dir,
+                                    '{}.png'.format(img_filename+'_'+str(self.img_row)+'_'+str(self.img_col)))
         if not os.path.exists(self.out_dir):
             os.makedirs(self.out_dir)
         print('Writing ... ' + img_save_path)
         cv2.imwrite(img_save_path, img_out)
-        
+
         return img_out
-        
+
 if __name__ == "__main__":
-    
-    parser = ArgumentParser(description = 'Class to preprocess and perform change detection')
+
+    parser = ArgumentParser(description = 'Class to preprocess images and perform change detection')
     parser.add_argument('--img0_path', type=str, help='path to first image')
     parser.add_argument('--img1_path', type=str, help='path to second image')
+    parser.add_argument('--img_row', type=int, default=0, help='row index of upper left corner of image chip')
+    parser.add_argument('--img_col', type=int, default=0, help='column index of upper left corner of image chip')
     parser.add_argument('--out_dir' , type=str, help='path to output path')
     parser.add_argument('--model_path', type=str, help='path to trained .pth model')
-    parser.add_argument('--use_corr', type=bool, help='use correlation?')
+    parser.add_argument('--use_corr', type=bool, default=True, help='use correlation?')
+
     opt = parser.parse_args()
-    
-    change_det = ChangeDetect(opt.img0_path, opt.img1_path, opt.out_dir, opt.model_path, opt.use_corr)
+
+    change_det = ChangeDetect(opt.img0_path, opt.img1_path, opt.img_row,
+                              opt.img_col, opt.out_dir, opt.model_path, opt.use_corr)
     input_ = change_det.preprocess_image()
-    mask_pred, img_out = change_det.run_inference(input_, opt.model_path)
-    
-    
+    mask_pred, img_out = change_det.run_inference(input_)
