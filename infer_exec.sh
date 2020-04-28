da283dd426bea5b69ae41dfcc1d2dd36474ed325
Brian Robinson <bigrobinson@gmail.com>
2020-04-28 15:16:17 -0500

    Update infer_exec.sh

--- infer_exec.sh
+++ infer_exec.sh
@@ -1,8 +1,8 @@
 python3 infer.py \
---img0_path /home/bigrobinson/sscdnet/data/BrianData/Baseline/P1/20191002_081108_268.jpeg \
---img1_path /home/bigrobinson/sscdnet/data/BrianData/AfterMath/P1/20191002_083141_722.jpeg \
---out_dir /home/bigrobinson/sscdnet/data/test_output \
---model_path /home/bigrobinson/sscdnet/log/cscdnet/checkpointdir/set0/cscdnet-00050000.pth \
+--img0_path \
+--img1_path \
+--out_dir \
+--model_path \
 --use_corr True \
---img_row 940 \
---img_col 1392 \
+--img_row \
+--img_col \
